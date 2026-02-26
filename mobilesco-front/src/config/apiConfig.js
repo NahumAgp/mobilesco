d0fc71d1c8fdc1d9f597569528a81bc1c0304ed0
NahumAgp <nahum.aguilar.per@gmail.com>
2026-02-25 19:46:53 -0600

    B

--- mobilesco-front/src/config/apiConfig.js
+++ mobilesco-front/src/config/apiConfig.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
+
 // ========================================
 // URL BASE DEL BACKEND
 // ========================================
 export const API_BASE_URL = "http://localhost:8081";
 
 //Produccion
-=======
-export const API_BASE_URL = "http://localhost:8081";
-
-//PRODUCCION 
->>>>>>> 0e1d45d8257ac51e4737b6c473f9df25c8bbe1e0
 //export const API_BASE_URL = "";
